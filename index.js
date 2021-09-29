6c44816956b85155618e4dafb21c13141502593e
ioslh <ohislh@gmail.com>
2021-09-29 18:29:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const gen = () => Math.random().toString(16).slice(2)
 console.log('--- start ---')
 
 for(let i = 0; i < 1e5; i++) {
-  console.log(`${i} - ${gen()}`
+  console.log(`${i} - ${gen()}`)
 }
 
 console.log('--- done ---')
