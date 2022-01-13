83ffa737af451b141feff33b450db8f2079d963a
ioslh <ohislh@gmail.com>
2022-01-13 14:54:10 +0800

    chore: test github styled log

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const gen = () => Math.random().toString(16).slice(2)
+// const gen = () => Math.random().toString(16).slice(2)
 
 console.log('--- start ---')
 
-for(let i = 0; i < 1e5; i++) {
-  console.log(`${i} - ${gen()}`)
-}
+// for(let i = 0; i < 1e5; i++) {
+//   console.log(`[command]${i} - ${gen()}`)
+// }
 
+console.log('[command]command content')
+console.log('[info]info content')
+console.log('[error]error content')
+console.log('##[group]manual group')
+console.log('manual group content')
+console.log('##[endgroup]')
+
+console.groupCollapsed('autogroup')
+console.log('auto group content')
+console.groupEnd('autogroup')
 console.log('--- done ---')
+
